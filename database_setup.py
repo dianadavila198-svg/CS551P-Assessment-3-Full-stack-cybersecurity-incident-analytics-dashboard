91f71d818b23c5b827e52931357855e1d3e4bc70
dianadavila198-svg <dianadavila198@gmail.com>
2026-05-05 16:32:01 +0100

    Final Project Structure set up and data set upload to database (data_loader.py)

---
+++ database_setup.py
@@ -0,0 +1,57 @@
+#Handles automatic loading of data set into the Database, with Pandas library support. 
+
+import os
+import pandas as pd
+import sqlite3
+#os library allows python to interact with the file system
+from dotenv import load_dotenv
+
+#0. Load dynamic Configuration from .env
+load_dotenv()
+
+#ENVIRONMENT SETUP
+def import_all_data():
+    #Data base path from .env 
+    db_path = os.getenv('DATABASE_URL')
+
+    #ENSURE THE INSTANCE FOLDER EXISTS. This is especially for local files and configuration that shouldn´t.
+    # be commited to version control.    
+    if not os.path.exists('instance'):
+        os.makedirs('instance')
+        
+    #DATABASE CONNECTION 
+    #Using db_path variable to use the dynamic Data base connection specified on .env configuratino. 
+    conn = sqlite3.connect(db_path)
+
+    #DATA IMPORT AND TRANSFORMATION (Normalized tables)
+
+    #To use linked tables for the relational schema between tables.
+    # --- Table 1: Incidents (Main table)---
+    df_incidents = pd.read_csv(os.getenv('INCIDENTS_CSV'))
+    df_incidents.to_sql('incidents', conn, if_exists='replace', index=False) #index False prevents Pandas for adding an extra ID colum when is not requiered. 
+    print (f"SucCess {len(df_incidents)} records imported into 'incidents' table")
+
+
+    # --- Table 2: Countries (Lookup Table) ---
+    df_countries = pd.read_csv(os.getenv('COUNTRIES_CSV'))
+    #to_sql take the data from the CSV file and put into the DataBase table inside SQLLite. Instead of writing INSERT TO for all rows. 
+    df_countries.to_sql('countries', conn, if_exists='replace', index=False)
+    print (f"Sucess {len(df_countries)} records imported into 'countries' table")
+
+    # --- Table 3: Attack Types (Lookup Table) ---
+    df_attacks = pd.read_csv(os.getenv('ATTACK_TYPES_CSV'))
+    df_attacks.to_sql('attack_types', conn, if_exists='replace', index=False)
+    print (f"Sucess {len(df_attacks)} records imported into 'attack_types' table")
+
+    #VERIFICATION 
+    print("Veritifcation Complete")
+    print("Incidents Table Columns:", df_incidents.columns.tolist())
+    #tolist use to convert into a Python list, for after use of this output headers in the program. 
+
+    #5. CLOSE CONNECTION 
+    conn.close()
+
+#EXECUTION CONTROL
+#Ensures this block acts as a reusable module. Can be reuse by other components such as test.py as a whole module without triggering a full database import process automatically. 
+if __name__ == "__main__":
+    import_all_data()
